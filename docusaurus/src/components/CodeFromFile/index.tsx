5902fd5dadad7334fe495f1f7daf6f5467f1b856
bmeeder22 <bmeeder22@gmail.com>
2023-03-16 15:39:12 -0500

    Can get any arbitrary code

--- docusaurus/src/components/CodeFromFile/index.tsx
+++ docusaurus/src/components/CodeFromFile/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import CodeBlock from '@theme/CodeBlock';
-import MyComponentSource from '!!raw-loader!/../../examples/react/solana/offramp/src/App.tsx';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 export type Languages = 'react';
 export type Blockchains = 'solana';
 export type Product = 'offramp' | 'checkout';
 
 export default function CodeFromFile({language, blockchain, product, file}: {language: Languages; blockchain: Blockchains; product: Product; file: string;}) {
-  // TODO make this able to load the files dynamically
-  return <CodeBlock language="ts">{MyComponentSource}</CodeBlock>;
+  const { siteConfig } = useDocusaurusContext();
+  const [code, setCode] = useState<string>("");
+
+  const filePath = useMemo(() => {
+    return `code/examples/${language}/${blockchain}/${product}/${file}`
+  }, []);
+
+  const languageExt =  useMemo(() => {
+    return 'ts'; // TODO parse file extension
+  }, []);
+
+  const url = useMemo(() => {
+    return `${siteConfig.baseUrl}${filePath}`;
+  }, []);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.text())
+      .then((data) => setCode(data));
+  }, [filePath, siteConfig.baseUrl]);
+
+  return <CodeBlock language={languageExt}>{code}</CodeBlock>;
 }
