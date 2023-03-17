f66fed9ff14956e20e66062c4f76df0675210fd6
bmeeder22 <bmeeder22@gmail.com>
2023-03-17 08:41:33 -0500

    Lint

--- docusaurus/src/components/CodeFromFile/index.tsx
+++ docusaurus/src/components/CodeFromFile/index.tsx
@@ -1,20 +1,30 @@
 import React, {useEffect, useMemo, useState} from 'react';
 import CodeBlock from '@theme/CodeBlock';
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export type Languages = 'react';
 export type Blockchains = 'solana';
 export type Product = 'offramp' | 'checkout';
 
-export default function CodeFromFile({language, blockchain, product, file}: {language: Languages; blockchain: Blockchains; product: Product; file: string;}) {
-  const { siteConfig } = useDocusaurusContext();
-  const [code, setCode] = useState<string>("");
+export default function CodeFromFile({
+  language,
+  blockchain,
+  product,
+  file,
+}: {
+  language: Languages;
+  blockchain: Blockchains;
+  product: Product;
+  file: string;
+}) {
+  const {siteConfig} = useDocusaurusContext();
+  const [code, setCode] = useState<string>('');
 
   const filePath = useMemo(() => {
-    return `code/examples/${language}/${blockchain}/${product}/${file}`
+    return `code/examples/${language}/${blockchain}/${product}/${file}`;
   }, [language, blockchain, product, file]);
 
-  const languageExt =  useMemo(() => {
+  const languageExt = useMemo(() => {
     return 'ts'; // TODO parse file extension
   }, []);
 
@@ -24,8 +34,8 @@ export default function CodeFromFile({language, blockchain, product, file}: {lan
 
   useEffect(() => {
     fetch(url)
-      .then((response) => response.text())
-      .then((data) => setCode(data));
+      .then(response => response.text())
+      .then(data => setCode(data));
   }, [filePath, siteConfig.baseUrl]);
 
   return <CodeBlock language={languageExt}>{code}</CodeBlock>;
