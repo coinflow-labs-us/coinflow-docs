efcaaedc3820cba94a39ca56e65d7daa94332f7c
Zori <zori.egikyan@gmail.com>
2023-03-21 22:28:59 +0400

    removed unused dependencies and png files

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/coinflow-social-card.png',
       navbar: {
         title: 'Coinflow Docs',
         logo: {
