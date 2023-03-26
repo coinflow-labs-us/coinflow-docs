b3dafbd8176a6d2f293558df417bc0e1aa6b7ec0
bmeeder22 <bmeeder22@gmail.com>
2023-03-26 16:33:40 -0500

    Coinflow withdraw component

--- examples/react/solana/offramp/src/App.tsx
+++ examples/react/solana/offramp/src/App.tsx
@@ -1,11 +1,42 @@
+import {CoinflowEnvs, CoinflowWithdraw} from '@coinflowlabs/react';
+import {useConnection, useWallet} from '@solana/wallet-adapter-react';
+import {WalletMultiButton} from '@solana/wallet-adapter-react-ui';
 import React from 'react';
 import {Wallet} from './Wallet';
 
 function App() {
   return (
+    <Wallet>
       <Wallet>
-        <div className="App"></div>
+        <div className="App">
+          <WalletMultiButton />
+          <div
+            style={{
+              height: '100vh',
+            }}
+          >
+            <CoinflowContent />
+          </div>
+        </div>
       </Wallet>
+    </Wallet>
+  );
+}
+
+function CoinflowContent() {
+  const {connection} = useConnection();
+  const wallet = useWallet();
+
+  return (
+    <CoinflowWithdraw
+      wallet={wallet}
+      merchantId={process.env.REACT_APP_MERCHANT_ID as string}
+      env={process.env.REACT_APP_ENV as CoinflowEnvs}
+      connection={connection}
+      onSuccess={() => console.log('Withdraw Success')}
+      blockchain={'solana'}
+      email={'user-email@email.com'}
+    />
   );
 }
 
