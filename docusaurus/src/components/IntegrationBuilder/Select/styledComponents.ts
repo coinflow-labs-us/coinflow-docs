bf76fec01e5971f9db6de11c0cd9624a95533a97
Zori Yeghikyan <45311503+zori7@users.noreply.github.com>
2023-03-29 16:58:56 +0400

    fixed solana and polygon withdraw confings and change some styles (#73)

--- docusaurus/src/components/IntegrationBuilder/Select/styledComponents.ts
+++ docusaurus/src/components/IntegrationBuilder/Select/styledComponents.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import {devices} from '@site/src/utils/mediaQuery';
 
 export const FiltersWrapper = styled.div`
   display: flex;
@@ -8,43 +7,5 @@ export const FiltersWrapper = styled.div`
 
   & > :not(:last-child) {
     margin-right: 1rem;
-  }
-`;
-
-export const SplitView = styled.div`
-  display: flex;
-  flex-direction: row;
-
-  .split-left,
-  .split-right {
-    height: calc(100vh - 60px - 4rem);
-  }
-
-  .split-left {
-    width: 40%;
-    overflow-y: auto;
-  }
-
-  .split-right {
-    width: 60%;
-  }
-
-  .scrollable-code {
-    overflow-y: auto;
-    height: calc(100% - 50px);
-  }
-
-  .code-block {
-    border-radius: 0;
-  }
-
-  @media ${devices.xs} {
-    flex-direction: column;
-
-    .split-left,
-    .split-right {
-      width: 100%;
-      height: initial;
-    }
   }
 `;
