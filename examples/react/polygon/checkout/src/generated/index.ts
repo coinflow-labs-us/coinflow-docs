167e815dcf476b16b4c067afb60030157dbc53ea
Maydar <bmeeder22@gmail.com>
2024-02-13 10:15:47 -0600

    Update Dependencies (#725)

---
+++ examples/react/polygon/checkout/src/generated/index.ts
@@ -0,0 +1,4 @@
+export type { NftMinting } from "./contracts/NftMinting/NftMinting";
+export { NftMinting__factory } from "./factories/contracts/NftMinting/NftMinting__factory";
+export type { NftsExchange } from "./contracts/NftsExchange/NftsExchange";
+export { NftsExchange__factory } from "./factories/contracts/NftsExchange/NftsExchange__factory";
