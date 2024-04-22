c9d5f43c74f299494b6f472210fa39367af8a9f4
Tasha Lee <tashalee@Tashas-MBP.lan>
2024-04-22 10:20:55 -0500

    add eslint configs + prettier

--- examples/angular/base/checkout/checkout-app/src/app/app.component.spec.tsx
+++ examples/angular/base/checkout/checkout-app/src/app/app.component.spec.tsx
@@ -1,14 +1,14 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -17,13 +17,15 @@ describe('AppComponent', () => {
   it(`should have the 'checkout-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('checkout-app');
+    expect(app.title).toEqual("checkout-app");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, checkout-app');
+    expect(compiled.querySelector("h1")?.textContent).toContain(
+      "Hello, checkout-app",
+    );
   });
 });
--- examples/angular/base/checkout/checkout-app/src/app/app.component.tsx
+++ examples/angular/base/checkout/checkout-app/src/app/app.component.tsx
@@ -1,14 +1,13 @@
-import {Component} from '@angular/core';
-import { PurchaseComponent } from './purchase/purchase.component';
-import { CoinflowPurchaseComponent } from '@coinflowlabs/angular';
-
+import { Component } from "@angular/core";
+import { PurchaseComponent } from "./purchase/purchase.component";
+import { CoinflowPurchaseComponent } from "@coinflowlabs/angular";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [PurchaseComponent, CoinflowPurchaseComponent],
-  templateUrl: './app.component.html',
+  templateUrl: "./app.component.html",
 })
 export class AppComponent {
-  title = 'checkout-app';
+  title = "checkout-app";
 }
