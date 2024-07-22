6cd2440fd6ef2030f5a2dd239cf66e4e43d31d23
Jake Montgomery <jmont96@gmail.com>
2024-07-22 17:39:19 -0500

    polyfill vue

--- examples/vue/solana/checkout/src/main.ts
+++ examples/vue/solana/checkout/src/main.ts
@@ -1,11 +1,10 @@
 // main.js or main.ts
-import { createApp } from 'vue';
-import App from './App.vue';
-import { Buffer } from 'buffer';
-import process from 'process';
-import 'crypto-browserify';
+import { createApp } from 'vue'
+import App from './App.vue'
+import { Buffer } from 'buffer'
+import process from 'process'
 
-window.Buffer = Buffer;
-window.process = process;
+window.Buffer = Buffer
+window.process = process
 
-createApp(App).mount('#app');
+createApp(App).mount('#app')
--- examples/vue/solana/checkout/vite.config.ts
+++ examples/vue/solana/checkout/vite.config.ts
@@ -1,44 +1,24 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import dotenv from 'dotenv';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import dotenv from 'dotenv'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-dotenv.config();
+dotenv.config()
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx(), nodePolyfills()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'buffer': 'buffer',
-      'crypto': 'crypto-browserify',
-      'stream': 'stream-browserify',
-      'assert': 'assert',
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   optimizeDeps: {
     esbuildOptions: {
       define: {
         global: 'globalThis'
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin()
-      ]
-    }
-  },
-  define: {
-    'process.env': {
-      ...process.env,
+      }
     }
   }
-});
+})
