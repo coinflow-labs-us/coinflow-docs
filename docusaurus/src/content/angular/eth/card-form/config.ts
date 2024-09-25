a9b31183b9eeec58e3db4d945ac25269fd47cc94
Tasha Lee <tashalee@Tashas-MacBook-Pro.local>
2024-09-25 15:15:22 -0500

    run lint

--- docusaurus/src/content/angular/eth/card-form/config.ts
+++ docusaurus/src/content/angular/eth/card-form/config.ts
@@ -1,5 +1,5 @@
 export const files = [
   'card-form/package.json',
   'card-form/src/app/app.module.tsx',
-  'card-form/src/app/card-form/card-form.component.tsx'
+  'card-form/src/app/card-form/card-form.component.tsx',
 ];
